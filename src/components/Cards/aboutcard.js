7d374f1fe1936563bfb16d3612c65d8c9c6f27bc
Alphena <alphena@gmail.com>
2021-05-04 19:19:22 -0700

    updated styling

--- src/components/Cards/aboutcard.js
+++ src/components/Cards/aboutcard.js
@@ -3,8 +3,8 @@ import Card from 'react-bootstrap/Card';
 
 function AboutCard() {
     return(
-        <Card style={{width: '18rem'}}>
-            <Card.Body>
+        // <Card className="info">
+            <Card.Body className="about" >
                 <Card.Title>
                     About Me
                 </Card.Title>
@@ -12,7 +12,7 @@ function AboutCard() {
                 I am a full stack developer, with a passion for interative applications that make it easy to find and access information. I'm constantly fascinated in imagining what is possible, and striving to do and discover more. 
                 </Card.Text>
             </Card.Body>
-        </Card>
+        // </Card>
         )
 }
 
