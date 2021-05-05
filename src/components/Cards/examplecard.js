d6f7930ce7b4e7000f2fc826820bebd7d50f6da9
Alphena <alphena@gmail.com>
2021-05-05 00:29:27 -0700

    added projects

--- src/components/Cards/examplecard.js
+++ src/components/Cards/examplecard.js
@@ -2,23 +2,20 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 
 
-function ExampleCard(props){
-    return(
-       <>
-            <Card 
-            className='bg-dark text-white'
-            style={{width: '18rem', margin: '1rem'}}
+function ExampleCard(props) {
+    return (
+        <>
+            <Card
+                className='bg-dark text-white'
+                style={{ width: '18rem', margin: '1rem' }}
             >
-                <Card.Img src={props.image} alt={props.alttext} className="work"  />
-                <Card.ImgOverlay >
-                    <Card.Title>{props.project}</Card.Title>
-                    <Card.Text>{props.details}</Card.Text>
-                    <Card.Text><a href={props.deploy} target="__blank">Link to Deployed Application</a></Card.Text>
+                <Card.Img src={props.image} alt={props.alttext} className="work" />
+                <Card.Title>{props.project}</Card.Title>
+                <Card.Text>{props.details}</Card.Text>
+                <Card.Text><a href={props.deploy} target="__blank">Link to Deployed Application</a></Card.Text>
                 <Card.Text><a href={props.repo} target="__blank">Link to Repo</a></Card.Text>
-                </Card.ImgOverlay>
-                
             </Card>
-       </>
+        </>
     )
 }
 
