0978d44a976a90759d1f7ea23067d2d606941d87
Alphena <alphena@gmail.com>
2021-05-05 19:04:32 -0700

    restructure

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Header from './components/header';
+import Footer from './components/footer';
+import Navbar from './components/navbar'
+
 import About from "./pages/about";
 import Contact from './pages/contact';
 import Examples from './pages/examples';
 
 function App() {
   return (
+   
     <Router>
+    <Header />
+    <Navbar />
       <div>
           <Route exact path='/' component={About} />
           <Route  path='/About' component={About} />
           <Route  path='/Contact' component={Contact} />
           <Route  path='/Examples' component={Examples} />
       </div>
+      <Footer />
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/header';
-import Footer from './components/footer';
-import Navbar from './components/navbar'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Header />
-    <Navbar />
-    <App />
-    <Footer />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
